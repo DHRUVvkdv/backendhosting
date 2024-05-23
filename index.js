a497836867c7fc2a5d16f8aeb63190bba0c567c2
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2024-05-23 17:26:52 -0400

    Added basic files for hosting

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world, let's host this!");
+});
+
+app.listen(process.env.PORT || PORT, () => console.log(`Listening on ${PORT}`));
