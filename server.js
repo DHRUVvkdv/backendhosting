d1b0df499a39bb635c1c2d00e05f666e2b9424d1
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2024-05-26 17:08:36 -0400

    Added message

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/test", (req, res) => {
-  res.json({ message: "It's working 🤗" });
+  res.json({ message: "It's working 🤗: AMAN TERI MKC!!" });
 });
 
 app.listen(process.env.PORT || PORT, () => console.log(`Listening on ${PORT}`));
