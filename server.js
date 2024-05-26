64cad2830772a861d61fe761dac1383c6bccc772
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2024-05-26 17:04:07 -0400

    added the cors

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 const PORT = 8000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello world, let's host this!" });
 });
+
 app.get("/test", (req, res) => {
   res.json({ message: "It's working 🤗" });
 });
