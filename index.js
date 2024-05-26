4713a68ee612ec180ffc7bca09a08e1865b22590
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2024-05-26 15:50:18 -0400

    added a test path

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const PORT = 4000;
 app.get("/", (req, res) => {
   res.send("Hello world, let's host this!");
 });
+app.get("/test", (req, res) => {
+  res.send("<h1>It's working 🤗</h1>");
+});
 
 app.listen(process.env.PORT || PORT, () => console.log(`Listening on ${PORT}`));
